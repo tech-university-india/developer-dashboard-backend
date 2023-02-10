3098cf319acd1367ac4b53687ed45d12c4e588e3
Siddharth Sharma <64124175+sidx255@users.noreply.github.com>
2023-02-10 15:27:49 +0530

    Feature/auth: resolve migration conflicts (#4)
    * chore: add migration and ORM for userAuth
    
    * feat: add authentication
    
    ---------
    
    Co-authored-by: Rohit Yadav <me.rohit.ydv@gmail.com>

---
+++ tests/controllers/auth.test.js
@@ -0,0 +1,47 @@
+const auth = require("../../src/controllers/auth");
+jest.mock("../../src/services/auth.js");
+let services = require("../../src/services/auth.js");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+
+
+describe("auth", ()=>{
+  it("should give 400 status when token is not generated", async ()=>{
+    const mockReq = {
+      body:{
+
+      }
+    };
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    };
+
+    services.mockImplementation(()=>{
+      return "Invalid id or password.";
+    });
+      
+    await auth(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(400);
+  });
+ 
+  it("should give 200 status when token is generated", async ()=>{
+    const mockReq = {
+      body:{
+
+      }
+    };
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    };
+
+    services.mockImplementation(()=>{
+      return jwt.sign({ username: "abc" }, config.get("jwtPrivateKey"), {expiresIn: "20m"});
+    });
+  
+    await auth(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(200);
+  });
+});
---
+++ tests/services/auth.test.js
@@ -0,0 +1,64 @@
+const auth = require("../../src/services/auth");
+const UserAuth = require("../../src/models").userAuth;
+const bcrypt = require("bcrypt");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+
+describe("auth", () => {
+  it("should return JWT when correct credentials are given.", async () => {
+    const mockReqBody = {
+      username: "abc",
+      password: "xyz"
+    };
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(mockReqBody.password, salt);
+
+    const returnValue = {
+      username: "abc",
+      password: hashedPassword
+    };
+
+    jest.spyOn(UserAuth, "findOne").mockResolvedValue(returnValue);
+
+    const token = await auth(mockReqBody);
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    expect(decoded.username).toBe(returnValue.username);
+  });
+
+  it("should not return JWT when wrong password is given.", async () => {
+    const mockReqBody = {
+      username: "abc",
+      password: "xyz"
+    };
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash("123", salt);
+
+    const returnValue = {
+      username: "abc",
+      password: hashedPassword
+    };
+
+    jest.spyOn(UserAuth, "findOne").mockResolvedValue(returnValue);
+
+    const token = await auth(mockReqBody);
+    expect(token).toBe("Invalid id or password.");
+  });
+
+  it("should not return JWT when user is not registered.", async () => {
+    const mockReqBody = {
+      username: "abc",
+      password: "xyz"
+    };
+
+    const returnValue = undefined;
+
+    jest.spyOn(UserAuth, "findOne").mockResolvedValue(returnValue);
+
+    const token = await auth(mockReqBody);
+    expect(token).toBe("Invalid id or password.");
+  });
+
+});
