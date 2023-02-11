5ff8deff0a363553b88e64bd47075e22607efcbe
sanyam9 <sanyamxsharma@gmail.com>
2023-02-11 14:06:34 +0530

    chore: fix quotes

--- src/controllers/leaveController.js
+++ src/controllers/leaveController.js
@@ -1,4 +1,4 @@
-const leaveService = require("../services/leave");
+const leaveService = require('../services/leave');
 
 const createLeave = async (req, res) => {
   try {
@@ -11,7 +11,7 @@ const createLeave = async (req, res) => {
     return res.status(201).json({
       status: 201,
       data: newLeave,
-      message: "Succesfully Created Leave",
+      message: 'Succesfully Created Leave',
     });
   } catch (error) {
     return res.status(500).json({ status: 500, message: error.message });
@@ -25,7 +25,7 @@ const deleteLeave = async (req, res) => {
     return res.status(200).json({
       status: 200,
       data: deleted,
-      message: "Succesfully Deleted Leave",
+      message: 'Succesfully Deleted Leave',
     });
   } catch (error) {
     return res.status(500).json({ status: 500, message: error.message });
--- src/routes/dashRouter.js
+++ src/routes/dashRouter.js
@@ -1,20 +1,20 @@
 
-const Router = require("express").Router();
-const { getUsers, checkAuth } = require("../controllers/dashController");
-const adminController = require("../controllers/adminController");
-const { isAdmin } = require("../middleware/admin_access");
-const leaveController = require("../controllers/leaveController");
-const eventController = require("../controllers/eventController");
+const Router = require('express').Router();
+const { getUsers, checkAuth } = require('../controllers/dashController');
+const adminController = require('../controllers/adminController');
+const { isAdmin } = require('../middleware/admin_access');
+const leaveController = require('../controllers/leaveController');
+const eventController = require('../controllers/eventController');
 
 Router.get('/users', getUsers);
 Router.route('/users/create').post(isAdmin, adminController.createUser);
 Router.post('/users/auth', checkAuth);
 
-Router.post("/leaves", leaveController.createLeave);
-Router.delete("/leave/:id", leaveController.deleteLeave);
+Router.post('/leaves', leaveController.createLeave);
+Router.delete('/leave/:id', leaveController.deleteLeave);
 
-Router.post("/events", eventController.createEvent);
-Router.get("/events/:projectId", eventController.getEvents);
-Router.delete("/event/:id", eventController.deleteEvent);
+Router.post('/events', eventController.createEvent);
+Router.get('/events/:projectId', eventController.getEvents);
+Router.delete('/event/:id', eventController.deleteEvent);
 
 module.exports = Router;
