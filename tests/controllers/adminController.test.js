880a78933e1cb8eb8fec2df988c71f3d357655d0
Ashutosh Senapati <ashutosh_senapati@mckinsey.com>
2023-02-17 04:13:28 +0530

    test: admin controller

--- tests/controllers/adminController.test.js
+++ tests/controllers/adminController.test.js
@@ -5,10 +5,11 @@ describe('adminController', () => {
   it('should return 201 and created user if the user is created', async () => {
     jest.spyOn(adminService, 'createUser').mockResolvedValue({ fmno: 'fmno', firstname: 'firstname' });
     const req = { body: { fmon: 'fmno', firstname: 'firstname' } };
+
     const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
     await adminController.createUser(req, res);
     expect(res.status).toBeCalledWith(201);
-    expect(res.status().json).toBeCalledWith({ status: 201, data: { fmno: 'fmno', firstname: 'firstname' }, message: 'Succesfully  Created User' });
+    expect(res.json).toBeCalledWith({ fmno: 'fmno', firstname: 'firstname' });
   });
   it('should return 500 and error message if the user is not created', async () => {
     jest.spyOn(adminService, 'createUser').mockRejectedValue(new Error('error'));
