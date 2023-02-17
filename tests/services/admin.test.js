07099a2acdce4be31c925774d0e785956e3e1da8
Ashutosh Senapati <ashutosh_senapati@mckinsey.com>
2023-02-17 15:06:58 +0530

    test: auth

--- tests/services/admin.test.js
+++ tests/services/admin.test.js
@@ -2,12 +2,72 @@
 // Testing DB data creation
 const adminService = require('../../src/services/admin');
 const db = require('../../src/models/index');
-const { it } = require('node:test');
 
 describe('AdminService', () => {
-  describe('createUser', () => {
-    it('Should return created user', async () => {
-      jest.spyOn(db.user, 'create').mockResolvedValue({
+  // describe('createUser', () => {
+  //   it('Should return created user', async () => {
+  //     jest.spyOn(db.user, 'create').mockResolvedValue({
+  //       'id': 0,
+  //       'username': 'ashutosh_senapati',
+  //       'email': 'ashutosh_senapati@mckinsey.com',
+  //       'role': 'Admin',
+  //       'phoneno': '9777139671',
+  //       'fmno': 328533,
+  //       'firstname': 'Ashutosh',
+  //       'lastname': 'Senapati',
+  //       'github': 'github.com/ashutoshmck',
+  //       'createdAt': '2023-02-16T19:27:09Z',
+  //       'updatedAt': '2023-02-16T19:27:09Z'
+  //     });
+  //     const user = await adminService.createUser('ashutosh_senapati', 328533, 'Ashutosh', 'Senapati', 'ashutosh_senapati@mckinsey.com', '9777139671', 'Admin', 'ashutoshmck');
+  //     expect(user).toBeEqual({
+  //       'id': 0,
+  //       'username': 'ashutosh_senapati',
+  //       'email': 'ashutosh_senapati@mckinsey.com',
+  //       'role': 'Admin',
+  //       'phoneno': '9777139671',
+  //       'fmno': 328533,
+  //       'firstname': 'Ashutosh',
+  //       'lastname': 'Senapati',
+  //       'github': 'github.com/ashutoshmck',
+  //       'createdAt': '2023-02-16T19:27:09Z',
+  //       'updatedAt': '2023-02-16T19:27:09Z'
+  //     });
+  //   });
+  // });
+  describe('getUsers', () => {
+    it('Should return list of all users', async () => {
+      jest.spyOn(db.user, 'findAll').mockResolvedValue(
+        [{
+          'id': 0,
+          'username': 'ashutosh_senapati',
+          'email': 'ashutosh_senapati@mckinsey.com',
+          'role': 'Admin',
+          'phoneno': '9777139671',
+          'fmno': 328533,
+          'firstname': 'Ashutosh',
+          'lastname': 'Senapati',
+          'github': 'github.com/ashutoshmck',
+          'createdAt': '2023-02-16T19:27:09Z',
+          'updatedAt': '2023-02-16T19:27:09Z'
+        },
+        {
+          'id': 0,
+          'username': 'ashutosh_senapati',
+          'email': 'ashutosh_senapati@mckinsey.com',
+          'role': 'Admin',
+          'phoneno': '9777139671',
+          'fmno': 328533,
+          'firstname': 'Ashutosh',
+          'lastname': 'Senapati',
+          'github': 'github.com/ashutoshmck',
+          'createdAt': '2023-02-16T19:27:09Z',
+          'updatedAt': '2023-02-16T19:27:09Z'
+        }
+        ]
+      );
+      const users = await adminService.getUsers();
+      expect(users).toEqual([{
         'id': 0,
         'username': 'ashutosh_senapati',
         'email': 'ashutosh_senapati@mckinsey.com',
@@ -19,9 +79,8 @@ describe('AdminService', () => {
         'github': 'github.com/ashutoshmck',
         'createdAt': '2023-02-16T19:27:09Z',
         'updatedAt': '2023-02-16T19:27:09Z'
-      });
-      const user = await adminService.createUser('ashutosh_senapati', 328533, 'Ashutosh', 'Senapati', 'ashutosh_senapati@mckinsey.com', '9777139671', 'Admin', 'ashutoshmck');
-      expect(user).toBeEqual({
+      },
+      {
         'id': 0,
         'username': 'ashutosh_senapati',
         'email': 'ashutosh_senapati@mckinsey.com',
@@ -33,7 +92,8 @@ describe('AdminService', () => {
         'github': 'github.com/ashutoshmck',
         'createdAt': '2023-02-16T19:27:09Z',
         'updatedAt': '2023-02-16T19:27:09Z'
-      });
+      }
+      ]);
     });
   });
 });
