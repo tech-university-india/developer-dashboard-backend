d8d3db446fc79318625a609ea94a576806f4777e
Balkar_Singh <balkar81199@gmial.com>
2023-03-06 00:09:38 +0530

    test: add test suite for team controller and validator

--- src/middlewares/teamValidator.js
+++ src/middlewares/teamValidator.js
@@ -13,16 +13,17 @@ const postTeamSchema = joi.array().items(
 
 const patchTeamSchema = joi.array().items(
   joi.object({
+    username: joi.string().required(),
     role: joi.string(),
     key_member: joi.boolean(),
     start_date: joi.date(),
     end_date: joi.date(),
     cost: joi.number()
-  })
+  }).required()
 );
 
 const teamValidator = (req, res, next) => {
-  const { error } = postTeamSchema.validate(req.body);
+  const { error } = postTeamSchema.validate(req.body.team_members);
   if (error) {
     res.status(400).json({ error: error.details[0].message });
   } else {
@@ -30,7 +31,7 @@ const teamValidator = (req, res, next) => {
   }
 };
 const patchValidator = (req, res, next) => {
-  const { error } = patchTeamSchema.validate(req.body);
+  const { error } = patchTeamSchema.validate(req.body.team_members);
   if (error) {
     res.status(400).json({ error: error.details[0].message });
   } else {
--- tests/middlewares/teamValidator.test.js
+++ tests/middlewares/teamValidator.test.js
@@ -2,129 +2,53 @@ const validateTeam = require('../../src/middlewares/teamValidator');
 
 describe('Team Validator', () => {
   describe('teamValidator', () => {
-    it('should throw bad request error when project_id is not valid', () => {
-      const req = { body: { project_id: 123, username: 'Balkar', adder_role: 'manager', emp_role: 'Developer', emp_status: 'active' } };
+    it('should throw bad request error when any of the fields is not valid', () => {
+      const req = {
+        query: { project_id: 'abc' },
+        body: { team_members: [{ username: 12, role: 'frontend', key_member: false, start_date: '2020-01-01', end_date: '2020-01-01', cost: 2000 }] },
+      };
       const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
       const next = jest.fn();
       validateTeam.teamValidator(req, res, next);
       expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"project_id" must be a string' });
-      expect(next).not.toHaveBeenCalled();
-    });
-    it('should throw bad request error when username is not valid', () => {
-      const req = { body: { project_id: '123', username: 123, adder_role: 'manager', emp_role: 'Developer', emp_status: 'active' } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-      const next = jest.fn();
-      validateTeam.teamValidator(req, res, next);
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"username" must be a string' });
-      expect(next).not.toHaveBeenCalled();
-    });
-    it('should throw erro when emp_role is not valid', () => {
-      const req = { body: { project_id: '123', username: 'Balkar', adder_role: 'manager', emp_role: 12, emp_status: 'active' } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-      const next = jest.fn();
-      validateTeam.teamValidator(req, res, next);
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"emp_role" must be a string' });
-      expect(next).not.toHaveBeenCalled();
-    });
-    it('should throw error when adder_role is not valid', () => {
-      const req = { body: { project_id: '123', username: 'Balkar', adder_role: 'developer', emp_role: 'Developer', emp_status: 'active' } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-      const next = jest.fn();
-      validateTeam.teamValidator(req, res, next);
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"adder_role" must be one of [manager, supermanager]' });
+      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"[0].username" must be a string' });
       expect(next).not.toHaveBeenCalled();
     });
     it('should call next when all the fields are valid', () => {
-      const req = { body: { project_id: '123', username: 'Balkar', adder_role: 'manager', emp_role: 'Developer' } };
+      const req = {
+        query: { project_id: 'abc' },
+        body: { team_members: [{ username: 'Balkar', role: 'frontend', key_member: false, start_date: '2020-01-01', end_date: '2020-01-01', cost: 2000 }] },
+      };
       const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
       const next = jest.fn();
       validateTeam.teamValidator(req, res, next);
-      expect(next).toHaveBeenCalled();
-    });
-  });
-  describe('getTeamValidator', () => {
-    it('should throw bad request error when project_id is not valid', () => {
-      const req = { body: { project_id: 123, key: 'all', value: 'abc' } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-      const next = jest.fn();
-      validateTeam.getTeamValidator(req, res, next);
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"project_id" must be a string' });
-      expect(next).not.toHaveBeenCalled();
-    });
-    it('should throw bad request error when key is not valid', () => {
-      const req = { body: { project_id: '123', key: 'abc', value: 'abc' } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-      const next = jest.fn();
-      validateTeam.getTeamValidator(req, res, next);
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"key" must be one of [username, emp_status, role, all]' });
-      expect(next).not.toHaveBeenCalled();
-    });
-    it('should throw bad request error when value is not valid for specific key', () => {
-      const req = { body: { project_id: '123', key: 'role', value: 'abc' } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-      const next = jest.fn();
-      validateTeam.getTeamValidator(req, res, next);
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"value" must be one of [developer, manager, supermanager]' });
-      expect(next).not.toHaveBeenCalled();
-    });
-    it('should call next when all the fields are valid', () => {
-      const req = { body: { project_id: '123', key: 'all', value: 'abc' } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-      const next = jest.fn();
-      validateTeam.getTeamValidator(req, res, next);
       expect(next).toHaveBeenCalled();
     });
   });
   describe('updateTeamValidator', () => {
-    it('should throw bad request error when project_id is not valid', () => {
-      const req = { body: { project_id: 123, username: 'Balkar', emp_role: 'Developer', emp_status: 'active' } };
+    it('should throw bad request error when any of the fields is not valid', () => {
+      const req = {
+        query: { project_id: 'abc' },
+        body: { team_members: [{ username: 12, role: 'frontend', cost: 2000 }] },
+      };
       const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
       const next = jest.fn();
       validateTeam.patchValidator(req, res, next);
       expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"project_id" must be a string' });
-      expect(next).not.toHaveBeenCalled();
-    });
-    it('should throw bad request error when username is not valid', () => {
-      const req = { body: { project_id: '123', username: 123, emp_role: 'Developer', emp_status: 'active' } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-      const next = jest.fn();
-      validateTeam.patchValidator(req, res, next);
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"username" must be a string' });
-      expect(next).not.toHaveBeenCalled();
-    });
-    it('should throw bad request error when emp_role is not valid', () => {
-      const req = { body: { project_id: '123', username: 'Balkar', emp_role: '12', emp_status: 'active' } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-      const next = jest.fn();
-      validateTeam.patchValidator(req, res, next);
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"emp_role" must be one of [developer, manager, supermanager]' });
-      expect(next).not.toHaveBeenCalled();
-    });
-    it('should throw bad request error when emp_status is not valid', () => {
-      const req = { body: { project_id: '123', username: 'Balkar', emp_role: 'developer', emp_status: '12' } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-      const next = jest.fn();
-      validateTeam.patchValidator(req, res, next);
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"emp_status" must be one of [rolled off, active]' });
+      expect(res.status().json).toHaveBeenCalledWith({ 'error': '"[0].username" must be a string' });
       expect(next).not.toHaveBeenCalled();
     });
     it('should call next when all the fields are valid', () => {
-      const req = { body: { project_id: '123', username: 'Balkar', emp_role: 'developer', emp_status: 'active' } };
+      const req = {
+        query: { project_id: 'abc' },
+        body: { team_members: [{ username: 'Balkar', role: 'frontend', key_member: false, start_date: '2020-01-01', end_date: '2020-01-01', cost: 2000 }] },
+      };
       const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
       const next = jest.fn();
       validateTeam.patchValidator(req, res, next);
       expect(next).toHaveBeenCalled();
     });
+
+
   });
 });
